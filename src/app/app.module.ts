bb04ca6522f8db1a7a73d7f4eb5c0dd1880dc5e9
mariamoradev <mariainfo150704@gmail.com>
2024-10-06 18:38:02 -0500

    add environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// ===FIRE===
+import { AngularFireModule } from '@angular/fire/compat';
+
+import { environment } from 'src/environments/environment.prod';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7P_krLP3stm6pRUAeWK_SlgJARbuMq54",
+    authDomain: "tallerionic-112b7.firebaseapp.com",
+    projectId: "tallerionic-112b7",
+    storageBucket: "tallerionic-112b7.appspot.com",
+    messagingSenderId: "187014816203",
+    appId: "1:187014816203:web:206bc48a5090665f54d64e"
+  }
 };
