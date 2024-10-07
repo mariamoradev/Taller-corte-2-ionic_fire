be7ac4a9416f469b6c92ec53fdec780d5aa82908
mariamoradev <mariainfo150704@gmail.com>
2024-10-07 16:13:25 -0500

    adding registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
   {
     path: 'sign-up',
     loadChildren: () => import('./pages/sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   }
 ];
 
