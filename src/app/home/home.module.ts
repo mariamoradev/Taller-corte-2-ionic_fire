9672890f8f116fe6a9f3ab25f29291871a1e8dfb
HabibSebGJ <habibsebastiangj@gmail.com>
2024-10-20 17:48:50 -0500

    Added task do not yet

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { TaskListComponent } from '../shared/components/task/task-list/task-list.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, TaskListComponent]
 })
 export class HomePageModule {}
